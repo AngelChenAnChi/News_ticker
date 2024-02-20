1063a53c84d09ad68a7871debc11c87484b017a1
angel <angel@DESKTOP-A53VL9L>
2024-02-20 14:47:52 +0800

    加入專案檔案。

---
+++ News_ticker/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace News_ticker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tlp_color = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_RUN = new System.Windows.Forms.Button();
+            this.timer_run = new System.Windows.Forms.Timer(this.components);
+            this.button_clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tlp_color
+            // 
+            this.tlp_color.BackColor = System.Drawing.Color.Lavender;
+            this.tlp_color.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tlp_color.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.tlp_color.ColumnCount = 8;
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_color.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.tlp_color.Location = new System.Drawing.Point(15, 31);
+            this.tlp_color.Name = "tlp_color";
+            this.tlp_color.RowCount = 1;
+            this.tlp_color.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlp_color.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlp_color.Size = new System.Drawing.Size(303, 36);
+            this.tlp_color.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(13, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "You can choose the color of the light！";
+            // 
+            // button_RUN
+            // 
+            this.button_RUN.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button_RUN.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_RUN.Font = new System.Drawing.Font("Cooper Black", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_RUN.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.button_RUN.Location = new System.Drawing.Point(564, 13);
+            this.button_RUN.Name = "button_RUN";
+            this.button_RUN.Size = new System.Drawing.Size(80, 50);
+            this.button_RUN.TabIndex = 2;
+            this.button_RUN.Text = "RUN";
+            this.button_RUN.UseVisualStyleBackColor = false;
+            this.button_RUN.Click += new System.EventHandler(this.button_RUN_Click);
+            // 
+            // timer_run
+            // 
+            this.timer_run.Interval = 150;
+            this.timer_run.Tick += new System.EventHandler(this.timer_run_Tick);
+            // 
+            // button_clear
+            // 
+            this.button_clear.BackColor = System.Drawing.Color.Turquoise;
+            this.button_clear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_clear.Font = new System.Drawing.Font("Cooper Black", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_clear.Location = new System.Drawing.Point(662, 13);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(80, 50);
+            this.button_clear.TabIndex = 3;
+            this.button_clear.Text = "Clear";
+            this.button_clear.UseVisualStyleBackColor = false;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.button_RUN);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tlp_color);
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "News ticker";
+            this.TransparencyKey = System.Drawing.Color.Silver;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlp_color;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_RUN;
+        private System.Windows.Forms.Timer timer_run;
+        private System.Windows.Forms.Button button_clear;
+    }
+}
+
---
+++ News_ticker/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace News_ticker
+{
+    public partial class Form1 : Form
+    {
+        Bitmap red, orange, yellow, green, blue, indigo, purple, white, black;
+        List<Bitmap> colorImage;
+        List<Button> colorButton;
+        PictureBox[,] LED;
+        List<int[]> pic = new List<int[]>();
+        int cPos;
+
+        // create color selections for LED lights
+        void colorChoose()
+        {
+            tlp_color.ColumnCount = 0;
+            tlp_color.RowCount = 1;
+
+            red = Properties.Resources.red;
+            orange = Properties.Resources.orange;
+            yellow = Properties.Resources.yellow;
+            green = Properties.Resources.green;
+            blue = Properties.Resources.blue;
+            indigo = Properties.Resources.indigo;
+            purple = Properties.Resources.purple;
+            white = Properties.Resources.white;
+            black = Properties.Resources.black;
+
+            colorImage = new List<Bitmap>();
+            colorImage.Add(red);
+            colorImage.Add(orange);
+            colorImage.Add(yellow);
+            colorImage.Add(green);
+            colorImage.Add(blue);
+            colorImage.Add(indigo);
+            colorImage.Add(purple);
+            colorImage.Add(white);
+
+            colorButton = new List<Button>();
+            for (int i = 0; i < 8; i++)
+            {
+                Button c = new Button();
+                colorButton.Add(c);
+                c.Click += colorButton_clicked;
+                tlp_color.Controls.Add(c, i, 0);
+                c.Image = colorImage[i];
+                c.BackgroundImageLayout = ImageLayout.Stretch;     
+                tlp_color.ColumnCount++;  
+            }
+        }
+
+        //create lights
+        void createLED()
+        {
+            black = Properties.Resources.black;
+            LED = new PictureBox[15, 30];
+            int w = ClientRectangle.Width / 30;
+            for (int r = 0; r < 15; r++)
+            {
+                for (int c = 0; c < 30; c++)
+                {
+                    LED[r, c] = new PictureBox();
+                    LED[r, c].Width = LED[r, c].Height = w;
+                    LED[r, c].Left = 10 + c * w;
+                    LED[r, c].Top = (3 + r) * w;
+                    LED[r, c].Image = black;
+                    LED[r, c].SizeMode = PictureBoxSizeMode.StretchImage;
+                    this.Controls.Add(LED[r, c]);
+                    LED[r, c].Click += LED_Clicked;
+                }
+            }
+        }
+
+
+
+        //create cycling pic
+        int[,] recordPic()
+        {
+            int[,] aPic = new int[15,30];
+            for(int r = 0; r < 15; r++)
+            {
+                for(int c = 0; c < 30; c++)
+                {
+                    if (LED[r, c].Image == red)
+                    {
+                        aPic[r,c] = 1;
+                    }
+                    else if (LED[r, c].Image == orange)
+                    {
+                        aPic[r,c] = 2;
+                    }
+                    else if (LED[r, c].Image == yellow)
+                    {
+                        aPic[r,c] = 3;
+                    }
+                    else if (LED[r, c].Image == green)
+                    {
+                        aPic[r,c] = 4;
+                    }
+                    else if (LED[r, c].Image == blue)
+                    {
+                        aPic[r,c] = 5;
+                    }
+                    else if (LED[r, c].Image == indigo)
+                    {
+                        aPic[r,c] = 6;
+                    }
+                    else if (LED[r, c].Image == purple)
+                    {
+                        aPic[r,c] = 7;
+                    }
+                    else if (LED[r, c].Image == white)
+                    {
+                        aPic[r,c] = 8;
+                    }
+                }
+            }
+            return aPic;
+        }
+
+        void showPic(int[,] pic) 
+        {
+
+            for (int r = 0; r < 15; r++)
+            {
+                for (int c = 0; c < 30; c++)
+                {
+                    if ((c + 1) < 30)
+                    {
+                        if (pic[r, c + 1] == 0) LED[r, c].Image = black;
+                        else if (pic[r, c + 1] == 1) LED[r, c].Image = red;
+                        else if (pic[r, c + 1] == 2) LED[r, c].Image = orange;
+                        else if (pic[r, c + 1] == 3) LED[r, c].Image = yellow;
+                        else if (pic[r, c + 1] == 4) LED[r, c].Image = green;
+                        else if (pic[r, c + 1] == 5) LED[r, c].Image = blue;
+                        else if (pic[r, c + 1] == 6) LED[r, c].Image = indigo;
+                        else if (pic[r, c + 1] == 7) LED[r, c].Image = purple;
+                        else if (pic[r, c + 1] == 8) LED[r, c].Image = white;
+                    }
+                    else
+                    {
+                        if (pic[r, 0] == 0) LED[r, c].Image = black;
+                        else LED[r, c].Image = colorImage[pic[r, 0] - 1];
+                    }                   
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();          
+            colorChoose();
+            createLED();  
+        }
+
+        //press the light
+        private void colorButton_clicked(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 8; i++)
+            {
+                if (sender == colorButton[i])
+                {
+                    cPos = i;
+                }
+            }
+        }
+        private void LED_Clicked(object sender, EventArgs e)
+        {
+            PictureBox light = (PictureBox)sender;
+          
+            if (light.Image == black)
+            {
+                light.Image = colorImage[cPos];
+            }         
+            else light.Image = black;
+        }
+
+        //run the light
+        bool isRun = true;
+        private void timer_run_Tick(object sender, EventArgs e)
+        {
+            if (isRun)
+            {
+                int[,] currentPic = recordPic();
+             
+                showPic(currentPic);
+            }
+        }
+        private void button_RUN_Click(object sender, EventArgs e)
+        {
+            timer_run.Enabled = !timer_run.Enabled;
+            if (timer_run.Enabled)
+            {
+                isRun = true;
+                button_RUN.Text = "STOP";
+            }
+            else button_RUN.Text = "RUN";
+        }
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            for (int r = 0; r < 15; r++)
+            {
+                for (int c = 0; c < 30; c++)
+                {
+                    LED[r, c].Image = black;
+                }
+            }
+
+        }
+    }
+}
